d149a94cb95b6dcbfadb82bb09df996dc9dc47d2
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-04-15 16:42:29 +0300

    create method to read the file

--- seip2022_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
+++ seip2022_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -1,13 +1,51 @@
 package gradeshistogram;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class HistogramGenerator
 {
+
+    public static ArrayList<Integer> readNumbers(String file_path){
+        
+        ArrayList<Integer> grades = new ArrayList<Integer>();
+
+        try {
+            File myObj = new File(file_path);
+            Scanner myReader = new Scanner(myObj);
+
+            while (myReader.hasNextLine()) {
+
+                int grade = Integer.parseInt(myReader.nextLine());
+                grades.add(grade);
+                System.out.println(grade);
+            }
+            myReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error while reading the file occurred.");
+            e.printStackTrace();
+            System.exit(0);
+          }
+          return grades;
+    }
+
+
+
+
+
+
+
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String file_path = args[0];
+        readNumbers(file_path);
     }
 }
