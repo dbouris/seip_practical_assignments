ed1f63b1029b1cc38092d31358e8d9a28dacaf03
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-04-16 14:48:14 +0300

    add comments to create the javadoc

--- seip2022_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
+++ seip2022_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -10,11 +10,25 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+
 /**
- * Hello world!
+ * The HistogramGenerator program is used to generate a
+ * Histogram graph with data from using a .txt file given 
+ * as input.
  *
+ * @author Dimitris Bouris
  */
+
 public class HistogramGenerator {
+
+    /**
+     * This method is used to read the numbers in a .txt file
+     * and add them in an Array
+     * 
+     * @param file_path the absolute path of the .txt file
+     * @return returns the numbers in an ArrayList
+     */
+
     public static ArrayList<Integer> readNumbers(String file_path) {
 
         ArrayList<Integer> grades = new ArrayList<Integer>();
@@ -37,61 +51,68 @@ public class HistogramGenerator {
         return grades;
     }
 
+    /**
+     * This method is used to render the Histogram Chart using
+     * the data given as input.
+     * It uses the JFreeChart library.
+     * 
+     * @param dataValues the frequencies of the numbers.
+     */
 
     public static void generateChart(int[] dataValues) {
-		/*
-		 * The XYSeriesCollection object is a set XYSeries series (dataset) that
-		 * can be visualized in the same chart
-		 */
-		XYSeriesCollection dataset = new XYSeriesCollection();
-		/*
-		 * The XYSeries that are loaded in the dataset. There might be many
-		 * series in one dataset.
-		 */
-		XYSeries data = new XYSeries("random values");
 
-		/*
-		 * Populating the XYSeries data object from the input Integer array
-		 * values.
-		 */
-		for (int i = 0; i < dataValues.length; i++) {
-			data.add(i, dataValues[i]);
-		}
+        //initialize the dataset to add the numbers
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        /*
+         * The XYSeries that are loaded in the dataset. There might be many
+         * series in one dataset.
+         */
+        XYSeries data = new XYSeries("random values");
 
-		// add the series to the dataset
-		dataset.addSeries(data);
+        for (int i = 0; i < dataValues.length; i++) {
+            data.add(i, dataValues[i]);
+        }
+        dataset.addSeries(data);
 
-		boolean legend = false; // do not visualize a legend
-		boolean tooltips = false; // do not visualize tooltips
-		boolean urls = false; // do not visualize urls
+        boolean legend = false; // do not visualize a legend
+        boolean tooltips = false; // do not visualize tooltips
+        boolean urls = false; // do not visualize urls
 
-		// Declare and initialize a createXYLineChart JFreeChart
-		JFreeChart chart = ChartFactory.createXYLineChart("Histogram", "Grades", "Frequencies", dataset,
-				PlotOrientation.VERTICAL, legend, tooltips, urls);
+        // Declare and initialize a createXYLineChart JFreeChart
+        JFreeChart chart = ChartFactory.createXYLineChart("Histogram", "Grades", "Frequencies", dataset,
+                PlotOrientation.VERTICAL, legend, tooltips, urls);
 
-		/*
-		 * Initialize a frame for visualizing the chart and attach the
-		 * previously created chart.
-		 */
-		ChartFrame frame = new ChartFrame("First", chart);
-		frame.pack();
-		// makes the previously created frame visible
-		frame.setVisible(true);
-	}
+        /*
+         * Initialize a frame for visualizing the chart and attach the
+         * previously created chart.
+         */
+        ChartFrame frame = new ChartFrame("First", chart);
+        frame.pack();
+        // makes the previously created frame visible
+        frame.setVisible(true);
+    }
 
-    public static int[] calculateFreq(ArrayList<Integer> gradeList){
+    /**
+     * This method is used to calculate the frequencies of the numbers
+     * given as input.
+     * 
+     * @param gradeList the list of the grades generated from readNumbers()
+     * @return returns the frequencies in an int table. freq[x] represents the
+     *         frequency of number x
+     */
+
+    public static int[] calculateFreq(ArrayList<Integer> gradeList) {
         int[] freq = new int[11];
-        
+
         for (int i = 1; i < freq.length; i++) {
             freq[i] = 0;
         }
 
-        for(int i = 0; i < gradeList.size(); i++) {
+        for (int i = 0; i < gradeList.size(); i++) {
             freq[gradeList.get(i)]++;
         }
         return freq;
     }
-
 
     public static void main(String[] args) {
         String file_path = args[0];
