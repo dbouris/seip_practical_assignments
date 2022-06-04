32778fdfbe1648a9c80fa22aa2ab6a6651446f1c
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-06-04 18:43:09 +0300

    refactor the SourceFileReader with Factory

---
+++ sourcecodeanalyzer/src/main/java/codeanalyzer/SourceFileReaderFactory.java
@@ -0,0 +1,15 @@
+package codeanalyzer;
+
+public class SourceFileReaderFactory {
+    public SourceFileReader getSourceFileReader(String FileType){
+        if(FileType.equals("web")){
+            return new SourceFileReaderWeb();
+        }
+        else if(FileType.equals("local")){
+            return new SourceFileReaderLocal();
+        }
+        else{
+            return null;
+        }
+    }
+}
