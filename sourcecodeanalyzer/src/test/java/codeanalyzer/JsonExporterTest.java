c698d77c944df0e04d68c8677552e8ce85a6b03f
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-06-05 21:13:06 +0300

    create the JsonExporterTest class

---
+++ sourcecodeanalyzer/src/test/java/codeanalyzer/JsonExporterTest.java
@@ -0,0 +1,30 @@
+package codeanalyzer;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.doCallRealMethod;
+
+public class JsonExporterTest {
+
+    @Test
+	public void testWriteJson() {
+		MetricsExporter mockedExporter = mock(JsonExporter.class);
+		// create an empty metrics content
+		Map<String, Integer> metrics = new HashMap<>();
+		String outputFilepath = "whatever-path";
+		
+		//this is a demo of how a mocked object can call a real method (partial mocking)
+		doCallRealMethod().when(mockedExporter).write(metrics, outputFilepath);
+		mockedExporter.write(metrics, outputFilepath);
+		//just verify that the method was executed/called
+		verify(mockedExporter).write(metrics, outputFilepath);
+	}
+
+}
