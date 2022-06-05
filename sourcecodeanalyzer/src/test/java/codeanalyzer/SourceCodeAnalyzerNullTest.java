b10ea1b57a25b051d392d021b982405fe2277315
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-06-05 23:18:57 +0300

    create a class to test the NullSource Code Analyzer

---
+++ sourcecodeanalyzer/src/test/java/codeanalyzer/SourceCodeAnalyzerNullTest.java
@@ -0,0 +1,43 @@
+package codeanalyzer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class SourceCodeAnalyzerNullTest {
+    
+    // create the SourceCodeAnalyzerNull object
+    SourceCodeAnalyzerNull sca = new SourceCodeAnalyzerNull();
+
+    @Test
+    public void testCalculateLOC() {
+        // test the calculateLOC method
+        try {
+            assertEquals(0, sca.calculateLOC(""));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCalculateNOM() {
+        // test the calculateNOM method
+        try {
+            assertEquals(0, sca.calculateNOM(""));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCalculateNOC() {
+        // test the calculateNOC method
+        try {
+            assertEquals(0, sca.calculateNOC(""));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
