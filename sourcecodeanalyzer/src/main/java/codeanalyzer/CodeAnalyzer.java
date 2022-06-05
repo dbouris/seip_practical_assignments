eca50c5f135626fb2ef4b1ebfc30a0b3e699ee56
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-06-05 11:49:38 +0300

    create the Facade class of the sourcecodeanalyzer library

---
+++ sourcecodeanalyzer/src/main/java/codeanalyzer/CodeAnalyzer.java
@@ -0,0 +1,43 @@
+package codeanalyzer;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CodeAnalyzer {
+    public void generateMetrics(String JavaSourceCodeInputFile, String sourceCodeAnalyzerType,
+            String SourceCodeLocationType, String OutputFilePath, String OutputFileType) throws IOException {
+        
+                // get the reader object
+                SourceFileReaderFactory sourceFileReaderFactory = new SourceFileReaderFactory();
+                SourceFileReader sourceFileReader = sourceFileReaderFactory.getSourceFileReader(SourceCodeLocationType);
+                
+                //get the analyzer object
+                SourceCodeAnalyzerFactory sourceCodeAnalyzerFactory = new SourceCodeAnalyzerFactory();
+                SourceCodeAnalyzer sourceCodeAnalyzer = sourceCodeAnalyzerFactory.getSourceCodeAnalyzer(sourceCodeAnalyzerType, sourceFileReader);
+
+                //get the metrics and save them in a Map Object
+                int loc = sourceCodeAnalyzer.calculateLOC(JavaSourceCodeInputFile);
+                int nom = sourceCodeAnalyzer.calculateNOM(JavaSourceCodeInputFile);
+                int noc = sourceCodeAnalyzer.calculateNOC(JavaSourceCodeInputFile);
+
+                Map<String, Integer> metrics = new HashMap<>();
+                metrics.put("loc", loc);
+                metrics.put("nom", nom);
+                metrics.put("noc", noc);
+
+                //get the exporter object
+                MetricsExporterFactory metricsExporterFactory = new MetricsExporterFactory();
+                MetricsExporter metricsExporter = metricsExporterFactory.getMetricsExporter(OutputFileType);
+
+                //create the file
+                metricsExporter.write(metrics, OutputFilePath);
+                
+                
+                
+                
+
+
+    }
+
+}
